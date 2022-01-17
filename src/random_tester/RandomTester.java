d1fbd5d0418d65f47716da6a2742f774a95bec3d
Cristina Pat <calota.cristina@gmail.com>
2022-01-17 14:18:05 +0000

    first commit

---
+++ src/random_tester/RandomTester.java
@@ -0,0 +1,44 @@
+package random_tester;
+
+import java.util.Random;
+
+public class RandomTester {
+	
+	// instance variables
+    private Random random;
+
+    /**
+     * Constructor for objects of class RamdomTester
+     */
+    public RandomTester()
+    {
+        // initialise instance variables
+        random = new Random();
+    }
+		
+	    /**
+	     * Describe the method
+	     *
+	     */
+	    public void printOneRandom()
+	    {
+	       System.out.println(random.nextInt());
+	       System.out.println();
+	    }
+	    
+	    /**
+	     * Describe the method
+	     *
+	     * @param 
+	     */
+	    public void printMultiRandom(int howMany)
+	    {   
+	        int i = 1;
+	        while(i < howMany+1) {
+	            System.out.println("The "+ i + " random number is: " + random.nextInt());
+	            System.out.println();
+	            i++;
+	        }
+	      }
+
+	}
